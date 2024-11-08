830b8c737c22cf6e1efb5ac4ccf79719a11ff9b1
IDA-yosuke-roboken <ida-y@roboken.iit.tsukuba.ac.jp>
2024-11-09 01:17:13 +0900

    Change param

--- launch/simple_pure_pursuit_launch.py
+++ launch/simple_pure_pursuit_launch.py
@@ -8,14 +8,18 @@ def generate_launch_description():
             executable='simple_pure_pursuit_controller',
             name='simple_pure_pursuit_controller',
             output='screen',
+            remappings=[
+                ('/cmd_vel', '/cmd_vel_parallel'),
+                ('/scan', '/integrated_mid'),
+            ],
             parameters=[
                 {
-                    'lookahead_distance': 0.5,
+                    'lookahead_distance': 1.0,
                     'deceleration_distance': 1.0,
-                    'stop_distance': 0.25,
-                    'max_linear_velocity': 0.3,
-                    'max_angular_velocity': 1.5,
-                    'obstacle_distance_threshold': 0.5,
+                    'stop_distance': 0.2,
+                    'max_linear_velocity': 0.5,
+                    'max_angular_velocity': 0.4,
+                    'obstacle_distance_threshold': 1.0,
                     'obstacle_width_threshold': 0.4,
                 }
             ]
